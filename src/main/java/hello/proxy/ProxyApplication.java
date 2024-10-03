230fd8344e89184ef39b6646021aa21db41c0cf7
이민규규ㄱ민미임이ㅇ <read1516@naver.com>
2024-10-03 21:23:37 +0900

    feat(섹션8): 빈 후처리기-적용
    * 빈을 생성할 때마다 적용했었던 중복코드들을 없애기 위해 스프링 빈 후처리기를 이용함
    
    1. 빈 후처리기 - 예제 코드1
    2.
    빈 후처리기 - 예제 코드2
    빈 후처리기 - 적용

--- src/main/java/hello/proxy/ProxyApplication.java
+++ src/main/java/hello/proxy/ProxyApplication.java
@@ -1,7 +1,6 @@
 package hello.proxy;
 
-import hello.proxy.config.v2_dynamicProxy.DynamicProxyFilterConfig;
-import hello.proxy.config.v3_proxyfactry.ProxyFactoryConfigV1;
+import hello.proxy.config.v4_postprocessor.BeanPostProcessorConfig;
 import hello.proxy.trace.logtrace.LogTrace;
 import hello.proxy.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.boot.SpringApplication;
@@ -11,7 +10,7 @@ import org.springframework.context.annotation.Import;
 
 //@Import({AppV2Config.class, AppV1Config.class})
 //@Import(ConcreteProxyConfig.class)
-@Import(ProxyFactoryConfigV1.class)
+@Import(BeanPostProcessorConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.proxy.app") //주의
 public class ProxyApplication {
 
