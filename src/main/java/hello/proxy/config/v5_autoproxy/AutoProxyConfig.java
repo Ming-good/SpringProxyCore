012b04fce9a1cb9e13bf4c9b6de29a6b90ed6841
이민규규ㄱ민미임이ㅇ <read1516@naver.com>
2024-10-06 18:12:35 +0900

    feat(섹션8): 스프링이 제공하는 빈 후처리기2
    1. AspectJ의 표현식 사용 (포인트컷에서 request* 등을 사용하면 모든 request가 다적용 됨으로 AspectJ의
    표현식을 사용)

--- src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
+++ src/main/java/hello/proxy/config/v5_autoproxy/AutoProxyConfig.java
@@ -5,6 +5,7 @@ import hello.proxy.config.AppV2Config;
 import hello.proxy.config.v3_proxyfactry.advice.LogTraceAdvice;
 import hello.proxy.trace.logtrace.LogTrace;
 import org.springframework.aop.Advisor;
+import org.springframework.aop.aspectj.AspectJExpressionPointcut;
 import org.springframework.aop.support.DefaultPointcutAdvisor;
 import org.springframework.aop.support.NameMatchMethodPointcut;
 import org.springframework.context.annotation.Bean;
@@ -14,12 +15,40 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({AppV1Config.class, AppV2Config.class})
 public class AutoProxyConfig {
-    @Bean
+//    @Bean
     public Advisor advisor1(LogTrace logTrace) {
         // 자동 프록시 생성기는 포인트 컷으로 프록시 적용 여부를 판단한다
         // 자동 프록시 생성기는 포인트 컷으로 어드바이스 적용 여부를 판단한다.
         NameMatchMethodPointcut pointcut = new NameMatchMethodPointcut();
         pointcut.setMappedNames("request*", "order*", "save*");
+
+        LogTraceAdvice logTraceAdvice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, logTraceAdvice);
+    }
+
+//    @Bean
+    public Advisor advisor2(LogTrace logTrace) {
+        /*
+        *  " *                  : 모든 반환타입 "
+        *  " hello.proxy.app..  : 해당 패키지와 그 하위 패키지
+        *  " *(..)              : '*'은 모든 메서드 이름, '(..)' 파라미터 상관 없음
+        * */
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..))");
+
+        LogTraceAdvice logTraceAdvice = new LogTraceAdvice(logTrace);
+        return new DefaultPointcutAdvisor(pointcut, logTraceAdvice);
+    }
+
+    @Bean
+    public Advisor advisor3(LogTrace logTrace) {
+        /*
+         *  " *                  : 모든 반환타입 "
+         *  " hello.proxy.app..  : 해당 패키지와 그 하위 패키지
+         *  " *(..)              : '*'은 모든 메서드 이름, '(..)' 파라미터 상관 없음
+         * */
+        AspectJExpressionPointcut pointcut = new AspectJExpressionPointcut();
+        pointcut.setExpression("execution(* hello.proxy.app..*(..)) && !execution(* hello.proxy.app..noLog(..))");
 
         LogTraceAdvice logTraceAdvice = new LogTraceAdvice(logTrace);
         return new DefaultPointcutAdvisor(pointcut, logTraceAdvice);
